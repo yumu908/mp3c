388251388e1e630c9e3ea84a7da449a7ae8900c6
yumu908 <908556332@qq.com>
2022-09-30 10:25:58 +0800

    print 改正

--- mp3c.py
+++ mp3c.py
@@ -25,31 +25,31 @@ def compress_mp3_file(mp3_file_path):
     if (not mp3_file_path.endswith(".mp3")): return
 
     output_file_path = "c_{}.mp3".format(os.path.splitext(basename(mp3_file_path))[0])
-    print "\n Processing {} ==> {}".format(mp3_file_path, output_file_path)
+    print ("\n Processing {} ==> {}".format(mp3_file_path, output_file_path))
 
     audio_file = AudioSegment.from_file(mp3_file_path, "mp3")
     frame_rate = audio_file.frame_rate
     bytes_per_sample = audio_file.sample_width
 
     if (frame_rate == 11025):
-        print " frame rate is already 11025, ignore."
+        print (" frame rate is already 11025, ignore.")
         return
 
-    print " frame_rate {} ==> {}".format(frame_rate, "11025")
+    print (" frame_rate {} ==> {}".format(frame_rate, "11025"))
     audio_file.export(output_file_path, format="mp3", parameters=["-ar", "11025"])
 
 
 ## check arguments and print usage
 if (len(sys.argv) < 2 or sys.argv[1] == '?'):
-    print "\nUsage:  " + sys.argv[0] + " [mp3_file]\nFor example: mp3c.py song.mp3 \n"
+    print ("\nUsage:  " + sys.argv[0] + " [mp3_file]\nFor example: mp3c.py song.mp3 \n")
     sys.exit(-1)
 
 mp3_files = [x for x in sys.argv[1:] if x.endswith(".mp3")]
 # mp3_files = filter(lambda x: x.endswith(".mp3"), sys.argv[1:])
 
-print "\n Files to precess: {}".format(mp3_files)
+print ("\n Files to precess: {}".format(mp3_files))
 
 for mp3_file in mp3_files:
     compress_mp3_file(mp3_file)
 
-print "\n DONE."
+print ("\n DONE.")
